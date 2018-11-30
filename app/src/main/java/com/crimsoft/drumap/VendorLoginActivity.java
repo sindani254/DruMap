380681757a2cd8bd766d78cde6ec4ab0290d9e11
unknown <sindani254@gmail.com>
2018-11-30 11:59:11 +0300

    added google maps

--- app/src/main/java/com/crimsoft/drumap/VendorLoginActivity.java
+++ app/src/main/java/com/crimsoft/drumap/VendorLoginActivity.java
@@ -35,7 +35,7 @@ public class VendorLoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user != null) {
-                    Intent intent = new Intent(VendorLoginActivity.this, MapActivity.class);
+                    Intent intent = new Intent(VendorLoginActivity.this, VendorMapActivity.class);
                     startActivity(intent);
                     finish();
                     return;
