99c60908301e9cbae77c5d0b09c0a993c4593c33
unknown <sindani254@gmail.com>
2018-11-30 06:30:08 +0300

    added user login

--- app/src/main/java/com/crimsoft/drumap/MainActivity.java
+++ app/src/main/java/com/crimsoft/drumap/MainActivity.java
@@ -1,13 +1,29 @@
 package com.crimsoft.drumap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mVendor, mCustomer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVendor = (Button) findViewById(R.id.vendor);
+        mCustomer = (Button) findViewById(R.id.customer);
+
+        mVendor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VendorLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
